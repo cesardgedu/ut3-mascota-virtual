8493568ae9c38de0f98252b9045251ca5f1ad5e9
Juanfd17 <121299576+Juanfd17@users.noreply.github.com>
2022-12-24 23:52:48 +0100

    Variables declaradas

--- src/mascota/RatoncitoFiuFiu.java
+++ src/mascota/RatoncitoFiuFiu.java
@@ -4,7 +4,13 @@ public class RatoncitoFiuFiu {
     //
     // La clase mascota.RatoncitoFiuFiu aun no tiene ningun atributo
     //
-
+    private String nombre; //nombre de la mascota
+    private int edad; //tiempo en segundos de la mascota
+    private int peso; //peso en gramos de la mascota
+    private int hambre; //hambre que tiene la mascota entre 0 (sin hambre) y 10 (mucha hambre)
+    private int suciedad; //nivel de suciedad de la mascota entre 0(limpio) y 100(sucio)
+    private int salud; //salud de la mascota entre 0(muero) y 100(sano)
+    private int energia; //energia de la mascota entre 0(apatico) y 100 (activo)
     public RatoncitoFiuFiu(String nombre, int i, int i1, int i2, int i3, int i4) {
         // Un objeto mascota.RatoncitoFiuFiu debería informar cuando nace...
     }
